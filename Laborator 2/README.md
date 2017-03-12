e7e743c9e999c27eb5b38544d82abe63874d6139
VasileCobilas <cobilas2013@mail.ru>
2017-03-12 17:05:11 +0200

    Graphics interface

---
+++ Laborator 2/README.md
@@ -0,0 +1,2 @@
+# Pasii efectuarii lucrarii de laboratot Nr.2:
+#Am creat interfata grafica  a aplicatiei "Calculator"
---
+++ Laborator 2/SourceCode/Calculator/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class MainBord : Form
+    {
+        float number01;
+        float number02;
+        float plusButtonCounter = 0;
+        float minusButtonCounter = 0;
+        float multiplyButtonCounter = 0;
+        float divideButtonCounter = 0;
+        int clickCounter = 0;
+
+        public MainBord()
+        {
+            InitializeComponent();
+            
+
+
+        }
+
+        private void plusButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void minusButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void multiplyButton_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void divideButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void nineButton_Click(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void eightButton_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+        private void sevenButton_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void sixButton_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void fiveButton_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void fourButton_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void threeButton_Click(object sender, EventArgs e)
+        {
+           
+
+        }
+
+        private void twoButton_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void oneButton_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void zeroButton_Click(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void pointButton_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void plusMinusButon_Click(object sender, EventArgs e)
+        {
+            
+        }
+       
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void clearenterButton_Click(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void radButton_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void putButton_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        
+    }
+}
