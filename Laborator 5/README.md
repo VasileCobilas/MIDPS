54c304709c6517b7aba70b861a5e0edfbe366236
VasileCobilas <cobilas2013@mail.ru>
2017-05-22 23:12:31 +0300

    Am realizat partea de front end a saitului web

---
+++ Laborator 5/README.md
@@ -0,0 +1,3 @@
+# Am analizat propunerile echipei;
+# Am implimentat ideile propuse pentru partea de front-end;
+# Am obtinut cunostinte noi in  manipularea cu Bootstrap-ul;
---
+++ Laborator 5/SourceCode/script/script.js
@@ -0,0 +1,11 @@
+var img1 = getElementById("d1");
+var img2 = getElementById("d2");
+var img3 = getElementById("d3");
+var img4 = getElementById("d4");
+var img5 = getElementById("d5");
+var butt = getElementById("butt");
+var gameHolst = getElementById("game-holst");
+
+butt.onclick = function() {
+	
+}
